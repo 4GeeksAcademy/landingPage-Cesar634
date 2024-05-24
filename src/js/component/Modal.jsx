51974d7014d24d8b0d79f4040505d6830227821e
César Jovanni Zaldivar Camps <132796806+Cesarzaldivar@users.noreply.github.com>
2024-05-24 03:12:08 +0000

    mi landinpage de Cesar

---
+++ src/js/component/Modal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Modal = () => {
+    return (
+        <div>
+            <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+                 Navbar content 
+            </nav>
+        </div>
+    )
+};
+
+export default Modal;
